73030f7f635ec3f8541e7bd9729b8d6f56bc72d7
sedov_al <epicsedov@gmail.com>
2023-11-01 22:43:16 +0100

    refs #0278 - lx.app.mouse.getPosition(lx.Rect contextWidget): Object{x, y}

--- src/js/app/components/client/Mouse.js
+++ src/js/app/components/client/Mouse.js
@@ -5,6 +5,21 @@ class Mouse extends lx.AppComponent {
     get x() { return _x; }
     get y() { return _y; }
 
+    getPosition(context = null) {
+        if (!context) {
+            return {
+                x: this.x,
+                y: this.y
+            };
+        }
+
+        let rect = context.getGlobalRect();
+        return {
+            x: this.x - rect.left,
+            y: this.y - rect.top
+        };
+    }
+    
     onReady() {
         document.body.addEventListener('mousemove', (e) => {
             _x = e.clientX;
